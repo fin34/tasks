92bf52ded06560982a6fb02084cd46ceb2bc8960
fin34 <p.bydolek@interia.pl>
2019-06-22 14:41:56 +0200

    task 24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -17,7 +17,7 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("task_url", "https://fin34.github.io");
+        context.setVariable("tasks_url", "https://fin34.github.io");
         context.setVariable("button", "Visit website");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
